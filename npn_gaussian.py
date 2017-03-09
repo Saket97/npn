9e9f5891248a4a103c0d11f3bc5507f0bf803d2f
parthsharma1996@gmail.com <parthsharma1996@gmail.com>
2017-03-09 13:46:29 +0530

    replaced tf.add and tf.subtract with + and -

--- npn_gaussian.py
+++ npn_gaussian.py
@@ -11,30 +11,30 @@ o_m = []
 o_s = []
 
 
-a_m.append() = tf.placeholder(tf.random_normal([784,1],mean= 0, stddev=1))
-a_s.append() = tf.placeholder(tf.random_normal([784,1],mean= 0, stddev=1))
-o_m.append() = tf.placeholder(tf.random_normal([784,1],mean= 0, stddev=1))
-o_s.append() = tf.placeholder(tf.random_normal([784,1],mean= 0, stddev=1))
+a_m.append( tf.placeholder(tf.float32, shape= (784,784)))
+a_s.append( tf.placeholder(tf.float32, shape= (784,784)))
+o_m.append(  tf.placeholder(tf.float32, shape= (784,784)))
+o_s.append(  tf.placeholder(tf.float32, shape= (784,784)))
 
 #L is the number of Hidden Layers here
 
 for l in range(1,L):
 
     #declaring the tensorflow variables
-    a_m.append() = tf.Variable(tf.random_normal([800,1],mean= 0, stddev=1))
-    a_s.append() = tf.Variable(tf.random_normal([800,1],mean= 0, stddev=1))
-    o_m.append() = tf.Variable(tf.random_normal([800,1],mean= 0, stddev=1))
-    o_s.append() = tf.Variable(tf.random_normal([800,1],mean= 0, stddev=1))
+    a_m.append(  tf.Variable(tf.random_normal([800,1],mean= 0, stddev=1)))
+    a_s.append(  tf.Variable(tf.random_normal([800,1],mean= 0, stddev=1)))
+    o_m.append(  tf.Variable(tf.random_normal([800,1],mean= 0, stddev=1)))
+    o_s.append(  tf.Variable(tf.random_normal([800,1],mean= 0, stddev=1)))
 
     #Equation 1
     a_m[l-1],a_s[l-1] = transformFunction( a_c[l-1], a_m[l-1]) 
-    o_m[l] = tf.add( tf.matmul(a_m[l-1],W_m[l-1]) , b_m[l] )
+    o_m[l] =  tf.matmul(a_m[l-1],W_m[l-1]) + b_m[l] 
 
     #Equation 2
     term_1 = tf.matmul(a_s[l-1],W_s[l])
     term_2 = tf.matmul(a_s[l-1], tf.mul( W_m[l-1], W_m[l-1] ) ) 
     term_3 = tf.matmul(tf.multiply( a_m[l-1], a_m[l-1]),W_s[l])
-    o_s[l] = tf.add( term1 , term_2, term_3 , b_s[l])
+    o_s[l] =  term1 + term_2+ term_3 + b_s[l]
 
     #Equation 3
     o_c[l],o_d[l] = transformFunctionInverse(o_m[l],o_s[l])
@@ -44,11 +44,10 @@ for l in range(1,L):
     root_term = tf.sqrt(tf.add(0.25,o_d_term))
 
     a_m_sigm_term = tf.sigmoid( tf.divide(o_c[l], root_term))
-    a_m[l] = tf.subtract( tf.multiply(2,a_m_sigm_term) ,1)
+    a_m[l] =  tf.multiply(2,a_m_sigm_term) +1
 
     a_s_sigm_term = Alpha*(o_c[l]+Beta)/tf.sqrt(1+c_square*tf.sqaure(Alpha)*o_d[l]) - tf.square(a_m)- tf.multiply(2,a_m)-1
 
-    a_s[l] = tf.subract(4*a_s_sigm_term - tf.square(a_m[l]) -2*a_m[l],1)
-
+    a_s[l] = 4*a_s_sigm_term - tf.square(a_m[l]) -2*a_m[l]-1
 
 
