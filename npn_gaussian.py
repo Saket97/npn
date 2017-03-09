83b17f276d8fab8e3375f0acbde3c2f936ff64ac
parthsharma1996@gmail.com <parthsharma1996@gmail.com>
2017-03-09 22:38:38 +0530

    Just need to add optimization next

--- npn_gaussian.py
+++ npn_gaussian.py
@@ -1,23 +1,28 @@
+#We have taken help from example code from https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/multilayer_perceptron.py 
 from math import pi
+import math
 import tensorflow as tf
 
 c_square = tf.constant(pi)
-Alpha = tf.constant(8-4*tf.sqrt(2))
-Beta = tf.constant(-0.5*tf.log(tf.sqrt(2)+1))
+Alpha = tf.constant(8-4*math.sqrt(2.0))
+Beta = tf.constant(-0.5*math.log(math.sqrt(2.0)+1))
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
 
+mnist = input_data.read_data_sets("data/", one_hot=True)
 
 a_m = []
 a_s = []
 o_m = []
 o_s = []
 
-
+#Parameters
 a_m.append( tf.placeholder(tf.float32, shape= (784,784)))
 a_s.append( tf.placeholder(tf.float32, shape= (784,784)))
-o_m.append(  tf.placeholder(tf.float32, shape= (784,784)))
-o_s.append(  tf.placeholder(tf.float32, shape= (784,784)))
 
 #L is the number of Hidden Layers here
+L =2
+batch_size = 32
 
 for l in range(1,L):
 
@@ -47,7 +52,7 @@ for l in range(1,L):
     a_m_sigm_term = tf.sigmoid( tf.divide(o_c[l], root_term))
     a_m[l] =  tf.multiply(2,a_m_sigm_term) +1
 
-    a_s_sigm_term = Alpha*(o_c[l]+Beta)/tf.sqrt(1+c_square*tf.sqaure(Alpha)*o_d[l]) - tf.square(a_m)- tf.multiply(2,a_m)-1
+    a_s_sigm_term = Alpha*(o_c[l]+Beta)/tf.sqrt(1+c_square*tf.square(Alpha)*o_d[l])
 
     a_s[l] = 4*a_s_sigm_term - tf.square(a_m[l]) -2*a_m[l]-1
 
@@ -61,7 +66,46 @@ term_2 = tf.matmul(a_s[L-1], tf.mul( W_m[L-1], W_m[L-1] ) )
 term_3 = tf.matmul(tf.multiply( a_m[L-1], a_m[L-1]),W_s[L])
 o_s[L] =  term1 + term_2+ term_3 + b_s[L]
 
+o_d_term = tf.multiply(c_square, o_d[l])
+root_term = tf.sqrt(tf.add(1,o_d_term))
+
+a_m[L] = tf.sigmoid( tf.divide(o_c[L], root_term))
 #Equation 3
 o_c[L],o_d[L] = transformFunctionInverse(o_m[L],o_s[L])
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=
+
+#defining the loss terms and final loss
+epsilon_tensor = epsilon*tf.ones([10,1])
+loss = 0.5*(tf.multiply( epsilon_tensor/o_d[L], tf.ones([10,1])) + 1/o_c[L]*(o_c[L] - y) - K + tf.log( o_d[L])*tf.ones([10,1]) - K*tf.log(epsilon))
+
+optimizer = tf.train.AdamOptimizer()
+
+optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_examples = int(mnist.train.num_examples)
+        # Loop over all batches
+        for i in range(total_examples):
+            x_train, y_train = mnist.train.next_batch(total_examplgs)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c = sess.run(loss, feed_dict={x: x_train, y: y_train})
+            # Compute average loss
+            avg_cost += c / total_batch
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1), "cost=", \
+                "{:.9f}".format(avg_cost))
+    print("Optimization Finished!")
+
+    # Test model
+    correct_prediction = tf.equal(tf.argmax(o_c[L], 1), tf.argmax(y, 1))
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
