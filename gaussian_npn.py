9286e863414d5cd4806d7247493a724d7510e8d8
Kanishk Gandhi <kanishk050296@gmail.com>
2017-03-23 21:24:40 +0530

    add train functionality and pipeline

--- gaussian_npn.py
+++ gaussian_npn.py
@@ -1,85 +1,121 @@
-#We have taken help from example code from https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/multilayer_perceptron.py
 from math import pi
 import math
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 # constants declaration
-c_square = tf.constant(pi)
-Alpha = tf.constant(8-4*math.sqrt(2.0))
-Beta = tf.constant(-0.5*math.log(math.sqrt(2.0)+1))
 num_hidden_units = 800
+num_train = 55000
+train_epoch = 1
 dim_inputs = 784
 units = [784,800,800,10]
 L = 3
+batch_size = 64
 
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
 
 mnist = input_data.read_data_sets("data/", one_hot=True)
 
-def transformFunction(x,y):
-    return x,y
 
-def transformFunctionInverse(x,y):
-    return x,y
+graph = tf.Graph()
+with graph.as_default():
+    c_square = tf.constant(pi)
+    Alpha = tf.constant(8-4*math.sqrt(2.0))
+    Beta = tf.constant(-0.5*math.log(math.sqrt(2.0)+1))
+    def transformFunction(x,y):
+        return x,y
 
-# output of each neuron
-o_m = ["dummy"]
-o_s = ["dummy"]
-o_c = ["dummy"]
-o_d = ["dummy"]
-# after applying activations
-a_m = [] # initialied with input
-a_s = [] # initialised with 0
-a_c = ["dummy"]
-a_d = ["dummy"]
-# Weights
-W_m = ["dummy"]
-W_s = ["dummy"]
-W_c = ["dummy"]
-W_d = ["dummy"]
-# bias
-b_m = ["dummy"]
-b_s = ["dummy"]
-b_c = ["dummy"]
-b_d = ["dummy"]
+    def transformFunctionInverse(x,y):
+        return x,y
 
-# declare  model
-#Input layer
-a_m.append(tf.placeholder(tf.float32, shape=[units[0],1]))
-a_s.append(tf.zeros(shape=[units[0],1]))
-for l in range(1,L+1):
-    a_m.append(0)
-    a_s.append(0)
-    a_c.append(0)
-    a_d.append(0)
-    o_m.append(0)
-    o_s.append(0)
-    o_c.append(0)
-    o_d.append(0)
-    b_m.append(weight_variable([units[l],1]))
-    b_s.append(weight_variable([units[l],1]))
-    W_m.append(weight_variable([units[l], units[l-1]]))
-    W_s.append(weight_variable([units[l], units[l-1]]))
-    W_c.append(weight_variable([units[l], units[l-1]]))
-    W_d.append(weight_variable([units[l], units[l-1]]))
+    def weight_variable(shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
 
-# L covers the output layer also, even though a_m[L] is not required
-# equations
-for l in range(1, L+1):
-    o_m[l] = tf.matmul(W_m[l], a_m[l-1]) + b_m[l]
-    o_s[l] = tf.matmul(W_s[l], a_s[l-1]) + tf.matmul(W_m[l]*W_m[l], a_s[l-1]) + tf.matmul(W_s[l], a_m[l-1]*a_m[l-1]) + b_s[l]
-    o_c[l], o_d[l] = transformFunctionInverse(o_m[l], o_s[l])
-    tmp = o_c[l]/((1+c_square*o_d[l])**0.5)
-    a_m[l] = tf.sigmoid(tmp)
-    tmp = Alpha*(o_c[l]+Beta)/((1+c_square*Alpha*Alpha*o_d[l])**0.5)
-    a_s[l] = tf.sigmoid(tmp) - a_m[l]*a_m[l]
-    print("val l : ",l,len(a_c),len(a_d),len(a_m),len(a_s))
-    a_c[l], a_d[l] = transformFunctionInverse(a_m[l], a_s[l])
+    image_batch = tf.placeholder(tf.float32,[batch_size,784])
+    label_batch = tf.placeholder(tf.float32,[batch_size,10])
 
-x = tf.placeholder(tf.float32, [None, 784])
-y_ = tf.placeholder(tf.float32, [None, 10])
-# y =
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+    # output of each neuron
+    o_m = ["dummy"]
+    o_s = ["dummy"]
+    o_c = ["dummy"]
+    o_d = ["dummy"]
+    # after applying activations
+    a_m = [] # initialied with input
+    a_s = [] # initialised with 0
+    a_c = ["dummy"]
+    a_d = ["dummy"]
+    # Weights
+    W_m = ["dummy"]
+    W_s = ["dummy"]
+    W_c = ["dummy"]
+    W_d = ["dummy"]
+    # bias
+    b_m = ["dummy"]
+    b_s = ["dummy"]
+    b_c = ["dummy"]
+    b_d = ["dummy"]
+
+    # declare  model
+    #Input layer
+
+
+    a_m.append(tf.placeholder(tf.float32, shape=[units[0],1]))
+    a_s.append(tf.zeros(shape=[units[0],1]))
+    for l in range(1,L+1):
+        a_m.append(0)
+        a_s.append(0)
+        a_c.append(0)
+        a_d.append(0)
+        o_m.append(0)
+        o_s.append(0)
+        o_c.append(0)
+        o_d.append(0)
+        b_m.append(weight_variable([units[l],1]))
+        b_s.append(weight_variable([units[l],1]))
+        W_m.append(weight_variable([units[l], units[l-1]]))
+        W_s.append(weight_variable([units[l], units[l-1]]))
+        W_c.append(weight_variable([units[l], units[l-1]]))
+        W_d.append(weight_variable([units[l], units[l-1]]))
+
+    def forward_pass(data):
+        a_m[0] = data
+        # L covers the output layer also, even though a_m[L] is not required
+        # equations
+        for l in range(1, L+1):
+            o_m[l] = tf.matmul(W_m[l], a_m[l-1]) + b_m[l]
+            o_s[l] = tf.matmul(W_s[l], a_s[l-1]) + tf.matmul(W_m[l]*W_m[l], a_s[l-1]) + tf.matmul(W_s[l], a_m[l-1]*a_m[l-1]) + b_s[l]
+            o_c[l], o_d[l] = transformFunctionInverse(o_m[l], o_s[l])
+            tmp = o_c[l]/((1+c_square*o_d[l])**0.5)
+            a_m[l] = tf.sigmoid(tmp)
+            tmp = Alpha*(o_c[l]+Beta)/((1+c_square*Alpha*Alpha*o_d[l])**0.5)
+            a_s[l] = tf.sigmoid(tmp) - a_m[l]*a_m[l]
+            #print("val l : ",l,len(a_c),len(a_d),len(a_m),len(a_s))
+            a_c[l], a_d[l] = transformFunctionInverse(a_m[l], a_s[l])
+        return o_c[l]
+
+
+    predictions=[]
+    for image in tf.unpack(image_batch):
+        image = tf.reshape(image,shape=[units[0],1])
+        predictions.append(tf.reshape(forward_pass(image),shape= [1,units[3]]))
+    predictions = tf.reshape(tf.pack(predictions),shape = [batch_size,10])
+    #print("Pred shape:",predictions)
+    correct_prediction = tf.equal(tf.argmax(label_batch,1), tf.argmax(predictions,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = label_batch,logits= predictions))/batch_size
+
+    optimizer = tf.train.AdamOptimizer().minimize(cross_entropy)
+
+
+init = tf.global_variables_initializer()
+with tf.Session(graph=graph) as sess:
+    print "Running Session"
+    sess.run(tf.global_variables_initializer())
+    print "Session initialized"
+    for epoch in range(train_epoch):
+        for step in range(num_train/batch_size):
+            x_train, y_train = mnist.train.next_batch(batch_size)
+            pred, acc,loss, _ = sess.run([predictions,accuracy,cross_entropy,optimizer],feed_dict={image_batch:x_train,label_batch:y_train})
+            print("Epoch:",epoch," Step:",step," acc: ",acc," loss:",loss)
+
