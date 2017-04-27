893fdbc799fcefd193397f484a6892f8c51bdd6d
Kanishk Gandhi <kanishk050296@gmail.com>
2017-04-28 01:43:09 +0530

    change train struct

--- rnn_npn_gauss.py
+++ rnn_npn_gauss.py
@@ -1,8 +1,51 @@
+import functools
 import numpy as np
 import tensorflow as tf
 import math
 from operator import add
+
+
+def doublewrap(function):
+    """
+    A decorator decorator, allowing to use the decorator to be used without
+    parentheses if not arguments are provided. All arguments must be optional.
+    """
+    @functools.wraps(function)
+    def decorator(*args, **kwargs):
+        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
+            return function(args[0])
+        else:
+            return lambda wrapee: function(wrapee, *args, **kwargs)
+    return decorator
+
+
+@doublewrap
+def define_scope(function, scope=None, *args, **kwargs):
+    """
+    A decorator for functions that define TensorFlow operations. The wrapped
+    function will only be executed once. Subsequent calls to it will directly
+    return the result so that operations are added to the graph only once.
+
+    The operations added by the function live within a tf.variable_scope(). If
+    this decorator is used with arguments, they will be forwarded to the
+    variable scope. The scope name defaults to the name of the wrapped
+    function.
+    """
+    attribute = '_cache_' + function.__name__
+    name = scope or function.__name__
+    @property
+    @functools.wraps(function)
+    def decorator(self):
+        if not hasattr(self, attribute):
+            with tf.variable_scope(name, *args, **kwargs):
+                setattr(self, attribute, function(self))
+        return getattr(self, attribute)
+    return decorator
+
+
+
 class Model:
+
     def __init__(self,data,target,word_dim=8000,hidden=128):
         self.input = data
         self.target = target
@@ -19,6 +62,7 @@ class Model:
     def transformFunctionInverse(x,y):
         return x,y
 
+    
     def npn_ops(self,weights,i):
         o = [None]*4
         a = [None]*4
@@ -32,6 +76,7 @@ class Model:
         a[2], a[3] = self.transformFunctionInverse(a[0], a[1])
         return [o,a]
 
+    
     def rnn_cell(self,inputs,state_old):
 
         U_weights = tf.get_variable("U_weights",[2,self.hidden_dim,self.word_dim],tf.random_normal_initializer())
@@ -46,13 +91,15 @@ class Model:
         out = npn_ops(V_weights,state)[0]
         return out,state
 
+    
     def length(sequence):
         used = tf.sign(tf.reduce_max(tf.abs(sequence), reduction_indices=2))
         length = tf.reduce_sum(used, reduction_indices=1)
         length = tf.cast(length, tf.int32)
         return length
 
-    def prediction(self,input):
+    
+    def prediction(self):
         with tf.variable_scope('word_embedding'):
             w_word = tf.get_variable(name = 'w_word', shape =[self.word_dim,self.embedding_size], initializer = tf.truncated_normal_initializer())
             b_word = tf.get_variable(name = 'b_word', shape =[1,self.embedding_size], initializer =tf.constant_initializer(0.1))
@@ -64,7 +111,7 @@ class Model:
         l = 0
         out_list = []
 
-        for sent in tf.unstack(input):
+        for sent in tf.unstack(self.input):
             state = [tf.zeros([self.hidden_dim]),tf.zeros([self.hidden_dim])]
             out_vocab = tf.zeros([self.word_dim])
             j = 0
@@ -90,28 +137,37 @@ class Model:
             i+=1
         return out_list
 
-    def optimize(self,input,target):
-        predictions = self.prediction(input)
-        loss = tf.nn.softmax_cross_entropy_with_logits(labels = target,logits = predictions)
+    
+    def optimize(self):
+
+        predictions = self.prediction
+        loss = tf.nn.softmax_cross_entropy_with_logits(labels = self.target,logits = predictions)
         optimizer = tf.train.AdamOptimizer().minimize(loss)
         return loss
 
-    def train(self,input_file,target_file,epochs,batch_size):
-        X = np.load(input_file)
-        Y = np.load(target_file)
-        input = tf.placeholder(tf.float32, [None, 791])
-        target = tf.placeholder(tf.float32, [None, 791])
-        sess = tf.Session()
-        sess.run(tf.global_variables_initializer())
-        for epoch in range(train_epoch):
-            for step in range(num_train/batch_size):
-                offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
-                # Generate a minibatch.
-                batch_data = X[offset:(offset + batch_size), :]
-                batch_labels = Y[offset:(offset + batch_size), :]
-                pred,loss,_= sess.run([predictions,loss,optimizer],feed_dict={input:batch_data,target: batch_labels})
-                print("Epoch:",epoch," Step:",step," acc: ",acc," loss:",loss)
-        return
 
+    
     def error(self):
         return
+
+
+def train(input_file,target_file,epochs,batch_size):
+    X = np.load(input_file)
+    Y = np.load(target_file)
+    
+    num_train = X.shape[0]
+    input = tf.placeholder(tf.float32, [None, 791])
+    target = tf.placeholder(tf.float32, [None, 791])
+    m = Model(input,target)
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    
+    for epoch in range(epochs):
+        for step in range(num_train/batch_size):
+            offset = (step * batch_size) % (X.shape[0] - batch_size)
+            # Generate a minibatch.
+            batch_data = X[offset:(offset + batch_size), :]
+            batch_labels = Y[offset:(offset + batch_size), :]
+            loss= sess.run(m.optimize,feed_dict={input:batch_data,target: batch_labels})
+            print("Epoch:",epoch," Step:",step," loss:",loss)
+    return
